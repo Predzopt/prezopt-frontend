cd4008087c7f40679f16eb9f1100653a7f162c31
Dpsalmist <adebayoblessing407@gmail.com>
2025-09-27 14:28:23 +0100

    modify footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { FaDiscord, FaGithub, FaTelegram } from 'react-icons/fa';
@@ -15,26 +16,32 @@ export default function Footer() {
       />
 
       {/* GLOW CIRCLE */}
-      <div className="bg-main/5 absolute -top-1/2 left-1/2 aspect-square w-5/12 -translate-1/2 -translate-x-1/2 scale-x-200 rounded-full blur-3xl"></div>
+      <div className="bg-main/10 absolute -top-1/2 left-1/2 aspect-square w-5/12 -translate-1/2 -translate-x-1/2 scale-x-200 rounded-full blur-3xl"></div>
 
       <div className="mx-auto max-w-7xl">
         <div className="grid gap-8 md:grid-cols-2">
           <div className="w-full sm:w-1/2">
-            <h5 className="mb-4 font-semibold">Prezopt</h5>
-            <p className="text-white/60">
+             <Link href="/">
+                      <Image
+                        src="/images/brand/prezopt-new.svg"
+                        width={1174}
+                        height={263}
+                        alt="Prezopt Logo"
+                        className="w-[150px]"
+                      />
+                    </Link>
+            <p className="text-white/60 pt-5 pb-3">
               Non-custodial DeFi yield optimization protocol powered by machine learning.
             </p>
-            <div>
+            <div className="mt-4 flex gap-4">
               {communities.map(social => (
                 <Link
-                  className={cn(
-                    'size-10 rounded-xl shadow-[rgba(255,255,255,0.25)_0px_1px_2px_1px_inset]'
-                  )}
+                   className="size-11 inline-flex justify-center items-center rounded-[10px] px-2.5 text-white border-white/5 hover:border-main/10 hover:text-main  py-3.5 opacity-100 shadow-[inset_0_0.6px_0.6px_-1.58px_rgba(255,255,255,0.894),inset_0_2.28px_2.28px_-3.16px_rgba(255,255,255,0.745),inset_0_10px_10px_-4.75px_rgba(255,255,255,0.05)] backdrop-blur-[5px] duration-300 will-change-auto [background:linear-gradient(rgba(28,28,28,0.1)_0%,rgba(18,18,18,0.2)_100%)_rgba(0,0,0,0)]"
                   key={social.label}
                   href={social.url}
                   aria-label={social.label}
                 >
-                  <social.icon size={24} />
+                  <social.icon size={32} />
                 </Link>
               ))}
             </div>
@@ -59,7 +66,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="text-muted-foreground mt-8 border-t pt-8 text-center text-sm">
-          <p>&copy; 2024 Prezopt. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Prezopt. All rights reserved.</p>
         </div>
       </div>
     </footer>
@@ -75,7 +82,7 @@ const footerLinks = [
       { url: '/#', label: 'Governance' },
     ],
   },
-  {
+  {   
     category: 'Token',
     items: [
       { url: '/#', label: 'PZT Contract' },
